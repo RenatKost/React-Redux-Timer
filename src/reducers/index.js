0802279618fcb05f7a61d42a8d13a119af8b4c2f
RenatKost <renya.show@gmail.com>
2021-01-25 23:24:43 +0400

    restart

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import timerReducer from "./timersReducer";
-import selectedTimerReducer from "./selectedTimerReducer";
-
-export default combineReducers({
-  timers: timerReducer, // array
-  selectedTimer: selectedTimerReducer, // int/number
-});
